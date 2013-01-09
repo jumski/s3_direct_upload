b1c34051d8e56cb64008a6f3995b3ab064389dad
Wojtek Majewski <jumski@gmail.com>
2013-01-09 12:38:09 +0100

    Revert "Change uplaoder.url bucket syntax"
    This reverts commit db39fb2ce46cb64fb2c8398d4ecab5059be98596.

--- lib/s3_direct_upload/form_helper.rb
+++ lib/s3_direct_upload/form_helper.rb
@@ -54,7 +54,7 @@ module S3DirectUpload
       end
 
       def url
-        "https://s3.amazonaws.com/#{@options[:bucket]}"
+        "https://#{@options[:bucket]}.s3.amazonaws.com/"
       end
 
       def policy
